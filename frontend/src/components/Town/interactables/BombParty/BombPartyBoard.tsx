7c97b2e6a4db0359b93b332b4feea0323a78671e
George Doty <doty.g@northeastern.edu>
2024-04-01 21:46:33 -0400

    Prepping to Merge

--- frontend/src/components/Town/interactables/BombParty/BombPartyBoard.tsx
+++ frontend/src/components/Town/interactables/BombParty/BombPartyBoard.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BombPartyAreaController from '../../../../classes/interactable/BombPartyAreaController';
 import { InteractableID } from '../../../../types/CoveyTownSocket';
-
+import { TextBox } from './Components/TextBox';
 export type BombPartyGameProps = {
   gameAreaController: BombPartyAreaController;
 };
 
 export default function BombPartyBoard({ gameAreaController }: BombPartyGameProps): JSX.Element {
-  return <label> Hello World </label>;
+  const checkWord = (word: string): boolean => {
+    return word == 'car';
+  }
+
+  const [ textBoxText, setTextBoxText ] = useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTextBoxText(event.currentTarget.value);
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (checkWord(textBoxText)) {
+      console.log("That's a valid word!");
+    }
+    else {
+      console.log("Not correct.");
+    }
+
+    setTextBoxText("");
+  }
+
+  return <TextBox value={textBoxText} handleChange={handleChange} handleSubmit={handleSubmit}/>
 }
+
---
+++ frontend/src/components/Town/interactables/BombParty/Components/TextBox.tsx
@@ -0,0 +1,12 @@
+import { ChangeEventHandler, ReactEventHandler } from 'react';
+
+export function TextBox( props: {value?: string, handleChange?: ChangeEventHandler, handleSubmit?: ReactEventHandler} ): JSX.Element {
+  return <form onSubmit={props.handleSubmit}>
+    <input
+      value={props.value}
+      onChange={props.handleChange}
+      maxLength={20}
+      minLength={3}
+    />
+  </form>
+}
