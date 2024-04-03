30122521d5212cb1d30999bc4940c92b8eb49236
George Doty <doty.g@northeastern.edu>
2024-04-03 16:07:25 -0400

    Linter Again ...

--- frontend/src/components/Town/interactables/BombParty/Components/WarningLabel.tsx
+++ frontend/src/components/Town/interactables/BombParty/Components/WarningLabel.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 
-
-export default function WarningLabel(props: {children: string}) {
-    return <label> {props.children} </label>
+export default function WarningLabel(props: { children: string }) {
+  return <label> {props.children} </label>;
 }
